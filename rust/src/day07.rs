03df4830d41a311315859bdfb5ddbb6e6fd83531
Jacob Parish <jacobsparish@gmail.com>
2021-12-12 22:45:52 -0500

    Rust day 7
    all caught up to python :+1:

--- rust/src/day07.rs
+++ rust/src/day07.rs
@@ -1,7 +1,35 @@
 pub fn part_a(input: &str) -> i64 {
-    0
+    let crabs: Vec<i64> = input
+        .lines()
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let min = crabs.iter().cloned().min().unwrap_or(0);
+    let max = crabs.iter().cloned().max().unwrap_or(0);
+    (min..max + 1)
+        .map(|pos| crabs.iter().map(|crab| (crab - pos).abs()).sum())
+        .min()
+        .unwrap_or(0)
+}
+
+fn sum_to_n(n: i64) -> i64 {
+    n * (n + 1) / 2
 }
 
 pub fn part_b(input: &str) -> i64 {
-    0
+    let crabs: Vec<i64> = input
+        .lines()
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let min = crabs.iter().cloned().min().unwrap_or(0);
+    let max = crabs.iter().cloned().max().unwrap_or(0);
+    (min..max + 1)
+        .map(|pos| crabs.iter().map(|crab| sum_to_n((crab - pos).abs())).sum())
+        .min()
+        .unwrap_or(0)
 }
