16afc7142f27f5a2ba1829d2fc689475dbd9f10b
Jacob Parish <jacobsparish@gmail.com>
2021-12-26 14:36:47 -0500

    Update day17.rs

--- rust/src/day17.rs
+++ rust/src/day17.rs
@@ -14,6 +14,9 @@ fn parse_target_area(input: &str) -> (i64, i64, i64, i64) {
 
 pub fn part_a(input: &str) -> i64 {
     let (xmin, xmax, ymin, ymax) = parse_target_area(input);
+    // Doesn't necessarily work for all inputs - requires that there is some
+    // x-velocity for which we end up stationary within the target area. But
+    // hey, it works for my input, so ¯\_(ツ)_/¯
     -ymin * (-ymin - 1) / 2
 }
 
